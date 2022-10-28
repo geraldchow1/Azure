fc9d16ee505f9eca8e4c0f383b3c7946eebb4bae
Adil Touati <adiltouati@microsoft.com>
2022-10-28 10:22:30 -0400

    var group to Poc

--- create-ci.sh
+++ create-ci.sh
@@ -3,3 +3,9 @@ az pipelines create \
     --branch Adil \
     --yml-path .pipeline/az-test-ci.xyz \
     --skip-run
+
+az pipelines create \
+    --name tf-plan-ci \
+    --branch Adil \
+    --yml-path .pipeline/plan-ci.yml \
+    --skip-run
