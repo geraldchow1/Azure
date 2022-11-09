05b9a020dcef8bb28d2faef1ddcb31e4b03fdec2
Adil Touati <adiltouati@microsoft.com>
2022-11-09 10:27:03 -0500

    test granular trigger

--- use-modules/provider.tf
+++ use-modules/provider.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  skip_provider_registration = true
+  skip_provider_registration = false
   subscription_id = "c8515022-4cc9-4ed5-a3fe-182e05d732d8"
 }
 
